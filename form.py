bf876d9f95bf079c31704b5e4ad7d214757872cf
Iwlj4s <elizavetalenn@mail.ru>
2023-12-20 20:50:47 +0300

    Print result on page(will be better) and in console

--- form.py
+++ form.py
@@ -3,7 +3,7 @@ from wtforms import IntegerField, StringField, FloatField, SubmitField
 
 
 class SetsForm(FlaskForm):
-    user_a_value = FloatField('A = ')
-    user_b_value = FloatField('B = ')
+    user_a_value = StringField('A = ')
+    user_b_value = StringField('B = ')
 
     user_submit_btn = SubmitField('Get Res')
--- main.py
+++ main.py
@@ -13,9 +13,27 @@ app.config['SECRET_KEY'] = KEY
 
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/Home', methods=['GET', 'POST'])
 def index():
     form = SetsForm()
+    print('Before get result')
+
+    if request.method == 'POST' and form.validate_on_submit():
+        a = form.user_a_value.data.replace(',', '').split()
+        b = form.user_b_value.data.replace(',', '').split()
+
+        res_merge_a_b, res_intersection_a_b, res_difference_a_b, res_symm_diff_a_b = get_result(a, b)
+
+        print(res_merge_a_b)
+        print(res_intersection_a_b)
+        print(res_difference_a_b)
+        print(res_symm_diff_a_b)
+
+        return render_template('index.html', form=form,
+                               res_merge_a_b=res_merge_a_b,
+                               res_intersection_a_b=res_intersection_a_b,
+                               res_difference_a_b=res_difference_a_b,
+                               res_symm_diff_a_b=res_symm_diff_a_b)
+
     return render_template('index.html', form=form)
 
 
