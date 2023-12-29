b1179e9c10efe5697e21df424b5a88cf64f195ae
Iwlj4s <elizavetalenn@mail.ru>
2023-12-29 20:12:18 +0300

    Input working more correctly, but not well, next I should add ability to enter negative numbers

--- main.py
+++ main.py
@@ -20,29 +20,35 @@ app.config['SECRET_KEY'] = KEY
 def index():
     form = SetsForm()
 
-    check_user_enter = check_user_data(a=str(form.user_a_value.data), b=str(form.user_b_value.data))
-    check_user_enter_digit = check_user_digit(a=str(form.user_a_value.data), b=str(form.user_b_value.data))
-
     if request.method == 'POST' and form.validate_on_submit():
-        a = form.user_a_value.data.replace(',', '').split()     # Get User Input Value
-        b = form.user_b_value.data.replace(',', '').split()
+        # Init Check Functions #
 
-        if not check_user_enter_digit:
+        # Check user enter is not nothing
+        check_user_enter = check_user_data(a=form.user_a_value.data, b=form.user_b_value.data)
+
+        # Replace , and spaces for check user entry digit
+        check_user_enter_digit = check_user_digit(a=form.user_a_value.data.replace(',', '').replace(' ', ''),
+                                                  b=form.user_b_value.data.replace(',', '').replace(' ', ''))
+        # Checks #
+        if check_user_enter_digit:
             if not check_user_enter:
                 # If user Input digit
-                res_merge_a_b, res_intersection_a_b, res_difference_a_b, res_symm_diff_a_b = get_result(a, b)
+                a = form.user_a_value.data.replace(',', '').split()  # Get User Input Value
+                b = form.user_b_value.data.replace(',', '').split()
 
-                return render_template('index.html', form=form,
+                res_merge_a_b, res_intersection_a_b, res_difference_a_b, res_symm_diff_a_b = get_result(a, b)  # Get Res
+
+                return render_template('index.html', form=form,  # Return Res Data
                                        res_merge_a_b=res_merge_a_b,
                                        res_intersection_a_b=res_intersection_a_b,
                                        res_difference_a_b=res_difference_a_b,
                                        res_symm_diff_a_b=res_symm_diff_a_b, index_data=index_data)
 
-            elif check_user_data:   # If User Input without anything
+            elif check_user_data:  # If User Input without anything
                 form.user_a_value.data = "You should enter sets"
                 form.user_b_value.data = "You should enter sets"
 
-        else:   # If User input string
+        else:  # If User input string
             form.user_a_value.data = "You should enter digit"
             form.user_b_value.data = "You should enter digit"
 
--- main_functions/checking_user.py
+++ main_functions/checking_user.py
@@ -8,23 +8,17 @@ def check_user_data(a, b):
 
 # CHECK USER ENTER DIGIT #
 def check_user_digit(a, b):
-    a_digit = False
-    b_digit = False
+    a_digit = all(digit_a.isdigit() for digit_a in a)
+    b_digit = all(digit_b.isdigit() for digit_b in b)
 
-    for digit_a in a:
-        if digit_a.isdigit():
-            return a_digit == True
-        else:
-            return a_digit == False
-
-    for digit_b in b:
-        if digit_b.isdigit():
-            return b_digit == True
-        else:
-            return False
-
-    if a_digit and b_digit == True:
+    if a_digit == True and b_digit == True:
         return True
+
+    elif a_digit == False and b_digit == True:
+        return False
+
+    elif b_digit == False and a_digit == True:
+        return False
 
     else:
         return False
